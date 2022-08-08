5023e38ccc613189bfbd36baa28f0eabc580ee19
Junhyuk Hur <xuzhunhe@gmail.com>
2022-08-08 21:30:01 +0900

    Time: 3 ms (93.93%), Space: 13.8 MB (12.14%) - LeetHub

--- 69-sqrtx/69-sqrtx.swift
+++ 69-sqrtx/69-sqrtx.swift
@@ -1,10 +1,11 @@
 class Solution {
     func mySqrt(_ x: Int) -> Int {
+        if x < 2 { return x }
         var answer = 0
         var left = 0
-        var right = Int(x / 2 + 1)
+        var right = x / 2
         while left <= right {
-            var mid = Int((left + right) / 2)
+            var mid: Int = (left + right) / 2
             if mid * mid <= x {
                 answer = mid
                 left = mid + 1
