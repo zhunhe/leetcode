86bf452039399ebbe8449ba8c892e0e69955f4fe
Junhyuk Hur <xuzhunhe@gmail.com>
2022-08-08 20:35:54 +0900

    Time: 0 ms (100.00%), Space: 14.2 MB (44.20%) - LeetHub

--- 66-plus-one/66-plus-one.swift
+++ 66-plus-one/66-plus-one.swift
@@ -2,7 +2,8 @@ class Solution {
     func plusOne(_ digits: [Int]) -> [Int] {
         var carry = false
         var answer = digits
-        for i in stride(from: answer.count - 1, through: 0, by: -1) {
+        for i in (0 ..< answer.count).reversed() {
+        // for i in stride(from: answer.count - 1, through: 0, by: -1) {
             if answer[i] == 9 {
                 carry = true
                 answer[i] = 0
