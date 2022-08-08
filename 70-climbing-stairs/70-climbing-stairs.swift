10975f5b9dec8d1739075d5994a83153f0ee57e5
Junhyuk Hur <xuzhunhe@gmail.com>
2022-08-08 21:53:09 +0900

    Time: 3 ms (64.00%), Space: 13.8 MB (22.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.swift
+++ 70-climbing-stairs/70-climbing-stairs.swift
@@ -1,13 +1,14 @@
 class Solution {
     func climbStairs(_ n: Int) -> Int {
-        if n < 4 { return n }
-        var arr = [Int](repeating: 0, count: n + 1)
-        for i in 0 ..< 4 {
-            arr[i] = i
-        }
+        if n <= 3 { return n }
+        var before2 = 2
+        var before1 = 3
+        var answer = 5
         for i in 4 ... n {
-            arr[i] = arr[i - 2] + arr[i - 1]
+            answer = before2 + before1
+            before2 = before1
+            before1 = answer
         }
-        return arr[n]
+        return answer
     }
 }
