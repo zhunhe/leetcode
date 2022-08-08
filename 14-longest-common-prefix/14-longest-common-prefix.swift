7e8108093b2497e3dbb60ab73c7429e421fddae4
Junhyuk Hur <xuzhunhe@gmail.com>
2022-08-08 13:55:24 +0900

    Time: 33 ms (34.60%), Space: 14.3 MB (68.90%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        guard strs.count > 1 else { return strs[0] }
+        let sorted = strs.sorted{ $0.count < $1.count }
+        var answer = ""
+        for len in 0 ..< sorted[0].count {
+            var idx = sorted[0].index(sorted[0].startIndex, offsetBy: len)
+            let tmp = String(sorted[0][idx])
+            for i in 1 ..< sorted.count {
+                idx = sorted[i].index(sorted[i].startIndex, offsetBy: len)
+                if tmp != String(sorted[i][idx]) {
+                    return answer
+                }
+            }
+            answer += tmp
+        }
+        return answer
+    }
+}
