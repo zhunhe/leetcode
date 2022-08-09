97b5ff531b0bcbba628f16b56d6453076108917c
Junhyuk Hur <xuzhunhe@gmail.com>
2022-08-09 11:40:47 +0900

    Time: 47 ms (69.62%), Space: 15.1 MB (8.54%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        func dfs(_ node: TreeNode?) -> Int {
+            guard let cur = node else { return 0 }
+            let leftDepth = dfs(cur.left)
+            if leftDepth == -1 { return -1 }
+            let rightDepth = dfs(cur.right)
+            if rightDepth == -1 { return -1 }
+            
+            if abs(leftDepth - rightDepth) > 1 { return -1 }
+            return max(leftDepth, rightDepth) + 1
+        }
+        return dfs(root) != -1
+    }
+}
