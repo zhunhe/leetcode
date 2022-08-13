58d36ae346a29612f9492cf2f1ac4a26e482146b
Junhyuk Hur <xuzhunhe@gmail.com>
2022-08-13 19:06:00 +0900

    Time: 106 ms (99.36%), Space: 14.8 MB (50.80%) - LeetHub

--- 136-single-number/136-single-number.swift
+++ 136-single-number/136-single-number.swift
@@ -1,16 +1,8 @@
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        var dict: [Int: Int] = [:]
-        for num in nums {
-            if dict[num] == nil {
-                dict[num] = 1
-            } else {
-                dict.removeValue(forKey: num)
-            }
-        }
         var answer = 0
-        for (key, value) in dict {
-            answer = key
+        for num in nums {
+            answer ^= num
         }
         return answer
     }
