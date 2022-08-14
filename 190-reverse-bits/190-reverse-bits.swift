23cc594cf996eae1e4cb07290f2b1c323ceedaaa
Junhyuk Hur <xuzhunhe@gmail.com>
2022-08-14 14:31:54 +0900

    Time: 22 ms (7.28%), Space: 13.8 MB (47.68%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.swift
+++ 190-reverse-bits/190-reverse-bits.swift
@@ -1,7 +1,6 @@
 class Solution {
     func reverseBits(_ n: Int) -> Int {
         var s = ""
-        var idx = 0
         var n = n
         while (n > 0) {
             if n & 1 == 1 {
@@ -9,7 +8,6 @@ class Solution {
             } else {
                 s += "0"
             }
-            idx += 1
             n /= 2
         }
         while s.count < 32 {
