c36faecb41c878c6672db0c080457870081d694b
Junhyuk Hur <xuzhunhe@gmail.com>
2022-09-16 12:35:47 +0900

    Time: 12 ms (36.40%), Space: 14.6 MB (28.98%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func longestPalindrome(_ s: String) -> Int {
+        var hash: [Character: Int] = [:]
+        var hasOdd = false
+        var answer = 0
+        for c in s {
+            hash[c] = 1 + (hash[c] ?? 0)
+        }
+        for (key, value) in hash {
+            if value % 2 == 1 {
+                hasOdd = true
+                answer += (value - 1)
+            } else {
+                answer += value
+            }
+        }
+        return hasOdd ? answer + 1 : answer
+    }
+}
