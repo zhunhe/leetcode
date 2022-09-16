98eb6d1e283c383b117b4d5bfcbabb26c31a41d2
Junhyuk Hur <xuzhunhe@gmail.com>
2022-09-16 13:41:01 +0900

    Time: 753 ms (6.12%), Space: 14.9 MB (24.49%) - LeetHub

--- 561-array-partition/561-array-partition.swift
+++ 561-array-partition/561-array-partition.swift
@@ -1,10 +1,9 @@
 class Solution {
     func arrayPairSum(_ nums: [Int]) -> Int {
-        var nums = nums.sorted(by: <)
-        var answer = 0
-        for (index, num) in nums.enumerated() where index % 2 == 0 {
-            answer += num
+        var sum = 0
+        for (index, num) in nums.sorted(by: <).enumerated() where index % 2 == 0 {
+            sum += num
         }
-        return answer
+        return sum
     }
 }
