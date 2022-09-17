1ea710ba856c07209cff08dec2c549007cb8f915
Junhyuk Hur <xuzhunhe@gmail.com>
2022-09-17 10:57:40 +0900

    Time: 1373 ms (9.82%), Space: 18.8 MB (9.19%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.swift
+++ 217-contains-duplicate/217-contains-duplicate.swift
@@ -2,7 +2,7 @@ class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
         var dict = [Int: Int]()
         for index in 0 ..< nums.count {
-            if let find = dict[nums[index]] {
+            if dict[nums[index]] != nil {
                 return true
             }
             dict[nums[index]] = index
