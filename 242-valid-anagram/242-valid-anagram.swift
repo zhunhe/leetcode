43b2487436b024faacee8f8128516fa06a35eee9
Junhyuk Hur <xuzhunhe@gmail.com>
2022-09-17 13:15:10 +0900

    Time: 56 ms (13.57%), Space: 16.5 MB (7.86%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.swift
+++ 242-valid-anagram/242-valid-anagram.swift
@@ -1,7 +1,5 @@
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-        var s = s.sorted()
-        var t = t.sorted()
-        return s == t
+        return s.sorted() == t.sorted()
     }
 }
