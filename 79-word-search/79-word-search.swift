e3f8cb2b91a3ed3f6ef71f5ffa0db22557ac4cbe
Junhyuk Hur <xuzhunhe@gmail.com>
2022-09-23 14:42:47 +0900

    Time: 145 ms (98.44%), Space: 14.8 MB (6.61%) - LeetHub

--- 79-word-search/79-word-search.swift
+++ 79-word-search/79-word-search.swift
@@ -1,36 +1,35 @@
 class Solution {
     func exist(_ board: [[Character]], _ word: String) -> Bool {
-        let h = board.count
-        let w = board[0].count
         let word = Array(word)
-        var isVisited = Array(repeating: Array(repeating: false, count: w), count: h)
+        var isVisited = Array(repeating: Array(repeating: false, count: board[0].count), count: board.count)
         
-        for y in 0 ..< h {
-            for x in 0 ..< w {
+        for y in 0 ..< board.count {
+            for x in 0 ..< board[0].count {
                 guard board[y][x] == word[0] else { continue }
-                if dfs(0, y, x) {
+                if dfs(board, &isVisited, word, 0, y, x) {
                     return true
                 }
             }
         }
-        func dfs(_ depth: Int, _ y: Int, _ x: Int) -> Bool {
+        return false
+    }
+    
+    func dfs(_ board: [[Character]], _ isVisited: inout [[Bool]], _ word: [Character], _ depth: Int, _ y: Int, _ x: Int) -> Bool {
             guard depth < word.count else { return true }
             guard y >= 0 &&
                   x >= 0 &&
-                  y < h &&
-                  x < w &&
+                  y < board.count &&
+                  x < board[0].count &&
                   !isVisited[y][x] &&
                   board[y][x] == word[depth]
             else { return false }
             
             isVisited[y][x] = true
-            var find = dfs(depth + 1, y - 1, x) ||
-                       dfs(depth + 1, y + 1, x) ||
-                       dfs(depth + 1, y, x - 1) ||
-                       dfs(depth + 1, y, x + 1)
+            var find = dfs(board, &isVisited, word, depth + 1, y - 1, x) ||
+                       dfs(board, &isVisited, word, depth + 1, y + 1, x) ||
+                       dfs(board, &isVisited, word, depth + 1, y, x - 1) ||
+                       dfs(board, &isVisited, word, depth + 1, y, x + 1)
             isVisited[y][x] = false
             return find
         }
-        return false
-    }
 }
