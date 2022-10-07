8877fabccdee311e7b073ace9ab8c48016d59164
Junhyuk Hur <xuzhunhe@gmail.com>
2022-10-07 17:53:15 +0900

    Time: 10 ms (99.84%), Space: 14.5 MB (73.74%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.swift
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.swift
@@ -1,11 +1,13 @@
 class Solution {
     func firstUniqChar(_ s: String) -> Int {
-        var hash = [Character: Int]()
-        for c in s {
-            hash[c] = (hash[c] ?? 0) + 1
+        let a = Int(Character("a").asciiValue!)
+        var count = [Int](repeating: 0, count: 26)
+
+        for c in s.utf8 {
+            count[Int(c) - a] += 1
         }
-        for (index, c) in s.enumerated() {
-            if let count = hash[c], count == 1 {
+        for (index, c) in s.utf8.enumerated() {
+            if count[Int(c) - a] == 1 {
                 return index
             }
         }
