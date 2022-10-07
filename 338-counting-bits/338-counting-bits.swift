789cfde2422955336a3b27120a302c18c4743106
Junhyuk Hur <xuzhunhe@gmail.com>
2022-10-07 17:01:58 +0900

    Time: 71 ms (62.96%), Space: 19.7 MB (14.81%) - LeetHub

--- 338-counting-bits/338-counting-bits.swift
+++ 338-counting-bits/338-counting-bits.swift
@@ -2,7 +2,7 @@ class Solution {
     func countBits(_ n: Int) -> [Int] {
         var array = [Int](repeating: 0, count: n + 1)
         for i in 0 ..< n + 1 {
-            array[i] = String(i, radix: 2).filter{$0 == "1"}.count
+            array[i] = array[i / 2] + i % 2
         }
         return array
     }
