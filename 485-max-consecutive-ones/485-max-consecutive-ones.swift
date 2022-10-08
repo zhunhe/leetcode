4b492cda1f0be366f670a34b3e0010b8eda39367
Junhyuk Hur <xuzhunhe@gmail.com>
2022-10-08 14:40:44 +0900

    Time: 532 ms (26.08%), Space: 14.6 MB (14.52%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.swift
+++ 485-max-consecutive-ones/485-max-consecutive-ones.swift
@@ -2,9 +2,8 @@ class Solution {
     func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
         var answer = 0
         var count = 0
-        let len = nums.count
-        for i in 0 ..< len {
-            if nums[i] == 0 {
+        for num in nums {
+            if num == 0 {
                 count = 0
             } else {
                 count += 1
