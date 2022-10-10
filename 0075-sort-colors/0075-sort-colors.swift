230da9ac98a822cf410f8729e9fd5408e9b62900
Junhyuk Hur <xuzhunhe@gmail.com>
2022-10-10 16:55:40 +0900

    Time: 0 ms (100.00%), Space: 13.9 MB (83.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.swift
+++ 0075-sort-colors/0075-sort-colors.swift
@@ -1,5 +1,11 @@
 class Solution {
     func sortColors(_ nums: inout [Int]) {
-        nums.sort()
+        var count = (zero: 0, one: 0, two: 0)
+        count.zero = nums.filter { $0 == 0 }.count
+        count.one = nums.filter { $0 <= 1 }.count
+        count.two = nums.filter { $0 <= 2 }.count
+        for i in 0 ..< count.zero { nums[i] = 0 }
+        for i in count.zero ..< count.one { nums[i] = 1 }
+        for i in count.one ..< count.two { nums[i] = 2 }
     }
 }
