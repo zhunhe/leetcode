3d680703235fb8c277a29c0a1a540b54b40cf744
Junhyuk Hur <xuzhunhe@gmail.com>
2022-10-17 14:11:21 +0900

    Time: 23 ms (82.21%), Space: 14.6 MB (15.95%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.swift
+++ 0496-next-greater-element-i/0496-next-greater-element-i.swift
@@ -1,18 +1,13 @@
 class Solution {
     func nextGreaterElement(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
         var hash = [Int: Int]()
-        for i in 0 ..< nums2.count {
-            for j in i+1 ..< nums2.count {
-                if nums2[j] > nums2[i] {
-                    hash[nums2[i]] = nums2[j]
-                    break
-                }
+        var stack = [Int]()
+        for num2 in nums2 {
+            while !stack.isEmpty && stack.last! < num2 {
+                hash[stack.removeLast()] = num2
             }
+            stack.append(num2)
         }
-        var answer = [Int]()
-        for num1 in nums1 {
-            answer.append(hash[num1] ?? -1)
-        }
-        return answer
+        return nums1.map { hash[$0] ?? -1 }
     }
 }
