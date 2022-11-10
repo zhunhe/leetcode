789d82ae53edd2a482a5a74b4dc82db4dc5affd9
Junhyuk Hur <xuzhunhe@gmail.com>
2022-11-10 12:33:08 +0900

    Time: 12 ms (17.11%), Space: 14.1 MB (60.53%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.swift
+++ 0709-to-lower-case/0709-to-lower-case.swift
@@ -2,7 +2,7 @@ class Solution {
     func toLowerCase(_ s: String) -> String {
         var answer = ""
         for c in s {
-            if let upper = c.asciiValue, "A" <= c && c <= "Z" {
+            if let upper = c.asciiValue, 65 <= upper && upper <= 90 {
                 answer.append(Character(UnicodeScalar(upper + 32)))
             } else {
                 answer.append(c)
