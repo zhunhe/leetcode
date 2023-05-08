ac9a1ab0d0bd3c3b2231ef6209133c22b73b81f4
Junhyuk Hur <xuzhunhe@gmail.com>
2023-05-08 20:13:00 +0900

    Time: 3 ms (100.00%), Space: 14.2 MB (50.00%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func timeRequiredToBuy(_ tickets: [Int], _ k: Int) -> Int {
+        var diff = 0
+        var rightCount = 0
+        for (i, _) in tickets.enumerated() {
+            diff += max(tickets[k] - tickets[i], 0)
+            if i > k && tickets[i] >= tickets[k] {
+                rightCount += 1
+            }
+        }
+        print(tickets.count * tickets[k], rightCount, diff)
+        return tickets.count * tickets[k] - rightCount - diff
+    }
+}
