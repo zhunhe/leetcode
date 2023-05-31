e72b722977787ffce9456853f031316323297caf
Junhyuk Hur <xuzhunhe@gmail.com>
2023-06-01 00:40:25 +0900

    Time: 10 ms (12.50%), Space: 14 MB (62.50%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.swift
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.swift
@@ -1,6 +1,5 @@
 class Solution {
     func specialArray(_ nums: [Int]) -> Int {
-        // let nums = nums.sorted(by: >)
         var v = Array(repeating: 0, count: 102)
         for num in nums {
             v[num > 100 ? 100 : num] += 1
