7134fd65542a92195c2d3c7bb55ec160d4eed6f4
Junhyuk Hur <xuzhunhe@gmail.com>
2023-06-01 00:34:39 +0900

    Time: 6 ms (62.50%), Space: 13.7 MB (100.00%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func specialArray(_ nums: [Int]) -> Int {
+        let nums = nums.sorted(by: >)
+        var v = Array(repeating: 0, count: 102)
+        for num in nums {
+            v[num > 100 ? 100 : num] += 1
+        }
+        for i in stride(from: 100, to: 0, by: -1) {
+            v[i] += v[i + 1]
+            if v[i] == i {
+                return i
+            }
+        }
+        return -1
+    }
+}
